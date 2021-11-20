bbcf1cbd2c9e8a3ecaeb05b8fc8300774009468c
beinjamin <jauresbeinjamin@gmail.com>
2021-11-20 01:49:36 +0100

    Suppression

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
