d5ce3e27c20e69bba628cb484cd276b1f0d20d6c
beinjamin <jauresbeinjamin@gmail.com>
2021-11-20 01:59:24 +0100

    Gestion du body

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ui_e_commerce/components/body.dart';
 
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
