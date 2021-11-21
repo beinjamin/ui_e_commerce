ceadb50835de84b5a85ab5ba257edd4e7fd4385b
beinjamin <jauresbeinjamin@gmail.com>
2021-11-21 20:07:31 +0100

    Mise en place du diaporame d'image

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -13,26 +13,55 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 3,
-            child: Column(
-              children: <Widget>[
-                Text(
-                  "Jaures.E-Commerce",
-                  style: TextStyle(
-                    fontSize: getProportionateScreenWidth(36),
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )
-              ],
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                itemBuilder: (context, index) => SplashContent(
+                  image: 'assets/images/splash_1.png',
+                  text: 'Bienvenu Chez Pavel Shop',
+                ),
+              ),
             ),
-          ),
-          Expanded(flex: 2, child: SizedBox()),
-        ],
+            Expanded(flex: 2, child: SizedBox()),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({
+    Key? key,
+    required this.text,
+    required this.image,
+  }) : super(key: key);
+  final String text, image;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Spacer(),
+        Text(
+          "PAVELSHOP",
+          style: TextStyle(
+            fontSize: getProportionateScreenWidth(36),
+            color: kPrimaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(text),
+        Spacer(flex: 2),
+        Image.asset(
+          image,
+          height: getProportionateScreenHeight(265),
+          width: getProportionateScreenHeight(235),
+        ),
+      ],
     );
   }
 }
